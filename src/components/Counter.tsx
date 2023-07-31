9d95c5787aea5aba66f333d80f81258be6bd7e2c
Yourui <yourui.ruan@gmail.com>
2023-07-31 17:03:28 -0400

    Added Component file structure. Added basic navbar. Added counter placeholder. Initial MVP 1 frontend
    Co-authored-by: Victor He <victorhe33@gmail.com>

---
+++ src/components/Counter.tsx
@@ -0,0 +1,11 @@
+export const Counter = () => {
+  return (
+    <nav className="flex items-center center">
+      <div className="flex items-center">
+        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          Counter : <span className="text-[hsl(280,100%,70%)]">0</span> waiting
+        </h1>
+      </div>
+    </nav>
+  )
+}
