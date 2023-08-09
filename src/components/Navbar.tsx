73759bffb1eae70c14481fc884c9d932febd146c
Yourui Ruan <118780080+YouruiR@users.noreply.github.com>
2023-08-09 14:45:19 -0400

    Indentation fix
    Co-authored-by: Victor He <victorhe33@gmail.com>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -33,8 +33,8 @@ const Navbar = () => {
                 </div>
                 <div>
                     <Link
-                    className=""
-                    href="https://discord.com/api/oauth2/authorize?client_id=1092281094591750175&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Fcallback%2Fdiscord&response_type=code&scope=identify%20email"
+                        className=""
+                        href="https://discord.com/api/oauth2/authorize?client_id=1092281094591750175&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Fcallback%2Fdiscord&response_type=code&scope=identify%20email"
                     >
                     <h3 className="text-2xl font-bold">Login</h3>
                     </Link>
