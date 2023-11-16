3f6ed22943b5235db8d766300b67ab0d3677bd29
Kevin Phan <112226540+KP824@users.noreply.github.com>
2023-11-16 11:38:45 -0800

    5/link db prisma discovery (#29)
    * Created test server and generated test tables using Prisma schema file
    
    * Saving to merge with Dev
    
    * Reworking prisma schema to fit new front end features
    
    * Migrated new schema table into database. Need to clear out old data from previous schema.
    
    * Updated notes on how to run prisma studio server
    
    * Adjusting schema to fit basic user creation. Create user function failing
    
    * Latest schema updated. testIndex file and queries working properly.
    
    * non working state
    
    * beginnings of api creation
    
    * testing routes
    
    * connected the front end to backend
    
    * Landing page successfully displays number of existing number of users within the database
    
    * Added routes to create user. Successfully tested on postman
    
    ---------
    
    Co-authored-by: dnecklesportfolio <dwayneaneckles@gmail.com>

---
+++ src/pages/posts.tsx
@@ -0,0 +1,3 @@
+export default function Posts() {
+  return <h1>Posts</h1>;
+}
---
+++ src/server/testIndex.mjs
@@ -0,0 +1,86 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // ...write Prisma Client Queries here
+
+  // create the User first
+  const user = await prisma.user.create({
+    data: {
+      name: "",
+      email: "",
+      bulletinBoardPost: {
+        create: {
+          description: "",
+        },
+      },
+    },
+  });
+
+  // Create defaulttestuser
+  const defaultTestUser = await prisma.user.create({
+    data: {
+      name: "Default Test User",
+      email: "DefaultTestUser@Testing.com",
+    },
+  });
+
+  console.log("User created:", user);
+  console.log("Test created:", defaultTestUser);
+
+  //console.log("Bulletin Board Post created:", bulletinBoardPost);
+
+  const allUsers = await prisma.user.findMany();
+  console.dir(allUsers);
+  // console.log('All users in DB: ', JSON.stringify(allUsers));
+
+  // } catch (error) {
+  //     console.error('Error in testIndex. Failed check within the main function:', error);
+  //   } finally {
+  //     await prisma.$disconnect();
+  //   }
+}
+
+//Commentingout the main function now to test exporting thebulletinPostfunction
+
+//main();
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+});
+
+// reads data only
+// const allUsers = await prisma.user.findMany({
+//   include: {
+//     posts: true,
+//     profile: true,
+//   },
+// })
+
+
+// // Log all fields
+// allUsers.forEach((user) => {
+//   console.log(`User: `, user);
+//   console.log(`Posts: `, user.posts);
+//   console.log(`Profile: `, user.profile);
+
+// })
+
+
+  //create the bulletinboard post
+  // const bulletinBoardPost = await prisma.bulletinBoard.create({
+  //   data: {
+  //     description: "Testing Prisma BulletinBoard Post Creation Thursday ",
+  //     owner: {
+  //       connect: {
+  //         id: user.id, // use the ID of the bulletin board post
+  //       },
+  //     },
+  //   },
+  // });
